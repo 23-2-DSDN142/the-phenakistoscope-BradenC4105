e1a67137a657ee5571380af44a0adf408d2ef819
BradenC4105 <Braden.cooper41@outlook.com>
2023-09-12 18:40:58 +1200

    Got it working I hope

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -3,34 +3,33 @@ const SLICE_COUNT = 10;
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(false);
+  pScope.draw_layer_boundaries(true);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
+  pScope.load_image_sequence("FrogJump" , "PNG", 5 )
+
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, 56, 186, 209);  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(faces);
-  layer1.mode( SWIRL(5) );
-  layer1.set_boundary( 200, 1000 );
+  var layer1 = new PLayer(FrogJump);
+  layer1.mode( RING );
+  layer1.set_boundary( 400, 1000 );
 
   var layer2 = new PLayer(squares);
   layer2.mode( RING );
   layer2.set_boundary( 0, 400 );
+
+  var layer3 = new PLayer(lilypad)
+  layer3.mode( RING )
+  layer3.set_boundary( 450, 950)
 }
 
-function faces(x, y, animation, pScope){
-  
-  scale(animation.frame*2);
-
-  ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
-
+function FrogJump(x, y, animation, pScope){
+  scale(0.35)
+  pScope.draw_image_from_sequence("FrogJump", 0, -2400, animation.frame);
 }
 
 function squares(x, y, animation, pScope){
@@ -40,10 +39,14 @@ function squares(x, y, animation, pScope){
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
 
-  fill(66, 135, 245)
+  fill(17, 64, 30)
   arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
 
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
 
+
+}
+
+function lilypad(x, y, animation, pScope){
+  fill(23, 166, 61)
+  rect(50-animation.frame*200,500,100,10)
 }
