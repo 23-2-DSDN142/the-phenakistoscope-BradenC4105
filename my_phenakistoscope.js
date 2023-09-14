d1a37d2705babd13a72a2a83a1c25fa9420efe65
BradenC4105 <Braden.cooper41@outlook.com>
2023-09-14 13:25:16 +1200

    Final

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -7,24 +7,30 @@ function setup_pScope(pScope){
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
   pScope.load_image_sequence("FrogJump" , "PNG", 5 )
-
+  pScope.load_image("Frog_Face" , "PNG");
 }
 
 function setup_layers(pScope){
 
   new PLayer(null, 56, 186, 209);  //lets us draw the whole circle background, ignoring the boundaries
+ 
+  var layer3 = new PLayer(Flowers)
+  layer3.mode( SWIRL(2) )
+  layer3.set_boundary( 450, 950)
 
    var layer2 = new PLayer(squares);
   layer2.mode( RING );
   layer2.set_boundary( 0, 400 );
 
-  var layer3 = new PLayer(lilypad)
-  layer3.mode( SWIRL(2) )
-  layer3.set_boundary( 450, 950)
+  
 
   var layer1 = new PLayer(FrogJump);
   layer1.mode( RING );
   layer1.set_boundary( 400, 1000 );
+
+  var layer1 = new PLayer(FrogFace);
+  layer1.mode( RING );
+  layer1.set_boundary( 0, 400 );
 }
 
 function FrogJump(x, y, animation, pScope){
@@ -46,7 +52,7 @@ function squares(x, y, animation, pScope){
 
 }
 
-function lilypad(x, y, animation, pScope){
+function Flowers(x, y, animation, pScope){
   angleMode(DEGREES)
   noStroke()
   fill(245, 149, 205)
@@ -61,5 +67,6 @@ rotate(45)
 ellipse(0, 10, 20, 100)
 }
 function FrogFace(x, y, animation, pScope){
-  
+  scale(0.1)
+  pScope.draw_image("Frog_Face",0,-3000-animation.wave(2)*100);
 }
